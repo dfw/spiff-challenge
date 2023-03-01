9902674c8c03dd3d676fdb1530655d0264e1f2e6
David Weiss <me@davidfweiss.com>
2023-02-28 20:02:08 -0500

    Add animation

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Exercise from '../exercise/Exercise';
 
 const ProgressBarExercise = () => {
@@ -17,17 +17,74 @@ export default ProgressBarExercise;
 
 // ----------------------------------------------------------------------------------
 
+const animationClasses = ['start-animation', 'finish-animation', 'fade-out'];
+
+const removeAnimationClasses = (el) => {
+    animationClasses.forEach((c) => {
+        el.classList.remove(c);
+    });
+};
+
 const Solution = () => {
+    const [requestInProgress, setRequestInProgress] = useState(false);
+    const [fadeOutProgressBarTimeoutId, setFadeOutProgressBarTimeoutId] =
+        useState();
+    const ref = useRef();
+
+    const handleStartRequest = () => {
+        // Do nothing if request has been started
+        if (requestInProgress) {
+            return;
+        }
+
+        if (fadeOutProgressBarTimeoutId) {
+            clearTimeout(fadeOutProgressBarTimeoutId);
+        }
+
+        removeAnimationClasses(ref.current);
+
+        setRequestInProgress(true);
+
+        ref.current.classList.add('start-animation');
+    };
+
+    const handleFinishRequest = () => {
+        // Do nothing if request has not been started
+        if (!requestInProgress) {
+            return;
+        }
+
+        ref.current.classList.add('finish-animation');
+
+        // Reset request
+        setTimeout(() => {
+            setRequestInProgress(false);
+        }, 1000);
+
+        // Fade out progress bar
+        const timeoutId = setTimeout(() => {
+            ref.current.classList.add('fade-out');
+        }, 4000);
+
+        setFadeOutProgressBarTimeoutId(timeoutId);
+    };
+
     return (
         <>
-            <div className="progress-bar">
+            <div ref={ref} className="progress-bar">
                 <div className="remaining-progress"></div>
             </div>
             <div className="button-group">
-                <button className="button semi-bold small green">
-                    Start Request
+                <button
+                    onClick={handleStartRequest}
+                    className="button semi-bold small green"
+                >
+                    {requestInProgress ? 'Loading...' : 'Start Request'}
                 </button>
-                <button className="button semi-bold small red">
+                <button
+                    onClick={handleFinishRequest}
+                    className="button semi-bold small red"
+                >
                     Finish Request
                 </button>
             </div>
